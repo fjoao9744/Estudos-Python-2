1994ca1b8473c3ab30f10e4faafc70e61fef224a
João Fernando Cosme Bolzani Cheroto <154237716+fjoao9744@users.noreply.github.com>
2024-12-19 11:20:00 -0300

    dunders

---
+++ Testes/objetos/dunder dict.py
@@ -0,0 +1,14 @@
+class Pessoa:
+    def __init__(self, nome, idade) -> None:
+        self.nome = nome
+        self.idade = idade
+
+jorge = Pessoa("jorge", 18)
+
+print(jorge.__dict__)
+
+jorge.__dict__["idade"] = 13 
+
+print(jorge.__dict__)
+
+
---
+++ Testes/objetos/dunder slots.py
@@ -0,0 +1,19 @@
+class Pessoa:
+    def __init__(self, nome, idade) -> None:
+        self.__nome = nome
+        self.__idade = idade
+        
+class PessoaSlots:
+    __slots__ = ["nome", "idade"] # Define quais atributos a classe pode ter
+    
+    def __init__(self, nome, idade) -> None:
+        self.nome = nome
+        self.idade = idade
+        
+from sys import getsizeof
+
+p1 = Pessoa("João", 30)
+p2 = PessoaSlots("João", 30)
+
+print(f"Tamanho de Pessoa: {getsizeof(p1.__dict__)} bytes")  # Usa dicionário interno
+print(f"Tamanho de PessoaSlots: {getsizeof(p2)} bytes")  # Sem dicionário interno(o __slots__ elimina ele)
